fbce43fdb8230ecb04008971ef63938d0338efb9
Daiane Geraldino <129515871+DaianeGeraldino@users.noreply.github.com>
2024-09-04 14:01:27 -0300

    fix: docs

--- aula4.c
+++ aula4.c
@@ -4,6 +4,78 @@
 
 int main (){
 
+    /*
+    Calculadora
+    */
+    int operacao;
+	float numero1, numero2, resultado;
+	
+	printf("***MENU***\n");
+	printf("1 - Somar\n");
+	printf("2 - Subtrair\n");
+	printf("3 - Multiplicar\n");
+	printf("4 - Dividir\n");
+	
+	printf("Informe a operacao que deseja realizar: ");
+	scanf("%d", &operacao);
+	
+	printf("Me informe o primeiro numero: ");
+	scanf("%f", &numero1);
+	printf("Me informe o segundo numero: ");
+	scanf("%f", &numero2);
+	
+	//opercao = 2
+	
+	if(operacao == 1){
+		
+		resultado = numero1 + numero2;
+		
+	} else if(operacao == 2){
+		
+		resultado = numero1 - numero2;
+		
+	} else if(operacao == 3){
+		
+		resultado = numero1 * numero2;
+		
+	} else if(operacao == 4){
+		
+		if(numero2 != 0){
+			
+			resultado = numero1 / numero2;
+			
+		} else{
+			
+			printf("Erro! Nao existe divisao por zero!\n");
+			exit(1);
+		}
+		
+	} else{
+		
+		printf("Voce eh um mane. Digitou uma operacao que nao existe! Otario!\n");
+		exit(2);
+	}
+	
+	printf("Resultado = %.2f\n", resultado);
+
+    /*
+	reprovacao por falta
+	- frequencia for menor que 75% => reprovado por falta
+	
+	reprovacao por nota
+	- se sua nota for maior ou igual a 6.0 => aprovado!
+	- se sua nota for menor que 6.0 e maior ou igual 
+	  a 2.0 => exame final
+	  - se sua nota do exame final for maior ou igual
+	  10 - nota => aprovado
+	  - senao => reprovado por nota
+	- se sua nota for menor que 2.0 => reprovado por nota
+	
+	usuario deve informar a frequencia em %
+	e a nota. se for para a final, deve informar
+	a nota do exame final.
+	*/
+    
     float nota, notaFinal, calculo;
     int frequencia;
 
